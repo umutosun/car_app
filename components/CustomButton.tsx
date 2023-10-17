15336f1991eba4c078c79ce56ac9e0ae9cb77344
Umut Tosun <umut35137@gmail.com>
2023-10-17 19:25:28 +0300

    First commity

---
+++ components/CustomButton.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+const CustomButton = () => {
+  return (
+    <button disabled={false} type={"button"} className={`custom-btn`}></button>
+  );
+};
+
+export default CustomButton;
