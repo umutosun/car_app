2a39c9ce21f896fbf055fbe6dd8814944866c643
Umut Tosun <umut35137@gmail.com>
2023-10-22 16:33:01 +0300

    Feat: searchmanufacturer

--- components/index.ts
+++ components/index.ts
@@ -4,4 +4,13 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 import SearchBar from "./SearchBar";
 import CustomFilter from "./CustomFilter";
-export { Hero, CustomButton, Navbar, Footer, SearchBar, CustomFilter };
+import SearchManufacturer from "./SearchManufacturer";
+export {
+  Hero,
+  CustomButton,
+  Navbar,
+  Footer,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
+};
