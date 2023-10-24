8bfacfe6355ed3a479a445eb492a2a5b8d6a5968
Umut Tosun <umut35137@gmail.com>
2023-10-24 17:09:06 +0300

    Feat: calculate car rent function

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -11,7 +11,18 @@ interface CardCarProps {
 const CarCard = ({ car }: CardCarProps) => {
   const { city_mpg, year, make, model, transmission, drive } = car;
 
-  return <div className="car-card group"></div>;
+  return (
+    <div className="car-card group">
+      <div className="car-card__content">
+        <h2 className="car-card__content-title">
+          {make} {model}
+        </h2>
+      </div>
+      <p>
+        <span>Car Rent...</span>
+      </p>
+    </div>
+  );
 };
 
 export default CarCard;
--- utils/index.ts
+++ utils/index.ts
@@ -12,3 +12,16 @@ export async function fetchCars() {
   const result = await response.json();
   return result;
 }
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+  const basePriceDay = 50;
+  const mileageFactor = 0.1;
+  const ageFactor = 0.05;
+
+  const mileageRate = city_mpg * mileageFactor;
+  const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+  const rentalRatePerDay = basePriceDay + mileageRate + ageRate;
+
+  return rentalRatePerDay.toFixed(0);
+};
