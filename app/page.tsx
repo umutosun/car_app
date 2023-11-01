969e09aeab50e9e400b39e4912d339432cd9a09d
Umut Tosun <umut35137@gmail.com>
2023-11-02 02:34:19 +0300

    Feat: custom filter

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 
 import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
 import { fetchCars } from "@/utils";
+import { fuels, yearsOfProduction } from "@/constants";
 
 export default async function Home({ searchParams }) {
   const allCars = await fetchCars({
@@ -26,8 +27,8 @@ export default async function Home({ searchParams }) {
         <div className="home__filters">
           <SearchBar />
           <div className="home__filter-container">
-            <CustomFilter title="fuel" />
-            <CustomFilter title="year" />
+            <CustomFilter title="fuel" options={fuels} />
+            <CustomFilter title="year" options={yearsOfProduction} />
           </div>
         </div>
         {!isDataEmpty ? (
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,16 +1,16 @@
 "use client";
 
-import { useState } from "react";
+import Image from "next/image";
+import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 
 import SearchManufacturer from "./SearchManufacturer";
-import Image from "next/image";
 
 const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
   <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
     <Image
-      src="/magnifying-glass.svg"
-      alt="magnifying glass"
+      src={"/magnifying-glass.svg"}
+      alt={"magnifying glass"}
       width={40}
       height={40}
       className="object-contain"
@@ -19,35 +19,41 @@ const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
 );
 
 const SearchBar = () => {
-  const [manufacturer, setManufacturer] = useState("");
+  const [manufacturer, setManuFacturer] = useState("");
   const [model, setModel] = useState("");
+
   const router = useRouter();
 
   const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (manufacturer === "" && model === "") {
-      return alert("Please fill in the search bar");
+
+    if (manufacturer.trim() === "" && model.trim() === "") {
+      return alert("Please provide some input");
     }
+
     updateSearchParams(model.toLowerCase(), manufacturer.toLowerCase());
   };
 
   const updateSearchParams = (model: string, manufacturer: string) => {
     const searchParams = new URLSearchParams(window.location.search);
+
     if (model) {
       searchParams.set("model", model);
     } else {
       searchParams.delete("model");
     }
 
-    if (model) {
+    if (manufacturer) {
       searchParams.set("manufacturer", manufacturer);
     } else {
       searchParams.delete("manufacturer");
     }
-    const newPathname = `${
+
+    const newPathName = `${
       window.location.pathname
     }?${searchParams.toString()}`;
-    router.push(newPathname, { scroll: false });
+
+    router.push(newPathName, { scroll: false });
   };
 
   return (
@@ -55,24 +61,24 @@ const SearchBar = () => {
       <div className="searchbar__item">
         <SearchManufacturer
           manufacturer={manufacturer}
-          setManufacturer={setManufacturer}
+          setManufacturer={setManuFacturer}
         />
         <SearchButton otherClasses="sm:hidden" />
       </div>
       <div className="searchbar__item">
         <Image
           src="/model-icon.png"
-          alt="car model icon"
           width={25}
           height={25}
           className="absolute w-[20px] h-[20px] ml-4"
+          alt="car model"
         />
         <input
           type="text"
           name="model"
           value={model}
           onChange={(e) => setModel(e.target.value)}
-          placeholder="Tiguan"
+          placeholder="Tiguan..."
           className="searchbar__input"
         />
         <SearchButton otherClasses="sm:hidden" />
